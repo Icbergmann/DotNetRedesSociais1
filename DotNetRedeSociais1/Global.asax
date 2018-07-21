6fd827281cac415c223fe8735e3c6b2030ac702e
lnleonardov <leovaz61@gmail.com>
2018-07-21 10:55:39 -0300

    projetoredessociais1

---
+++ DotNetRedeSociais1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetRedeSociais1.MvcApplication" Language="C#" %>
---
+++ DotNetRedeSociais1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetRedeSociais1.Startup))]
+namespace DotNetRedeSociais1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
